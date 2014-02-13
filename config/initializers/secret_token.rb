61126b23de3b85bd28e0cf4ea42e5eca4427177f
mpaniotov <elteck81@gmail.com>
2014-02-13 16:06:56 +0200

    16.07

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Galery::Application.config.secret_key_base = 'dfdcd33ccb2c94ce3ce37b52eedc3fd2b70be972608e57f2212fe99c84180ab52f52096e0286c22bce4c3c30f91e1d3e81ea8bb71fdbb0758e5df7c9425e1147'
