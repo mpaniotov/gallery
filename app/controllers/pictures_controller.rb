6a3c92a04e9296eff6ddeed60b17afe70143d149
mpaniotov <elteck81@gmail.com>
2014-03-03 14:23:39 +0200

    03.03.2014

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,7 +10,7 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:picture_id])
     @category = Category.where(:picture_id=>@picture.id)
     @like_for_chosen_picture = Like.where(:user_id =>current_user.id, :picture_id => @picture.id).first
-    @comments=Comment.preload(:picture, :user)
+    @comments=Comment.preload(:picture, :user).where(:picture_id=>@picture.id)
     if !params[:redirect]
       Action.create(:user_id=>current_user.id, :action_type=>"navigation", :data => {:url=>request.original_url,:id=>@picture.id,:description=>'Watch picture',:time=>Time.zone.now})
     end
--- test/controllers/actions_controller_test.rb
+++ test/controllers/actions_controller_test.rb
@@ -4,4 +4,5 @@ class ActionsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
 end
